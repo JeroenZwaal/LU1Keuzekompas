88add1518f4e44443cde60c70de96c8e0ca6cea1
Jeroen Zwaal <93530512+JeroenZwaal@users.noreply.github.com>
2025-10-18 23:54:51 +0200

    fix

--- Backend/api-keuze-kompass-lu1/src/application/Services/module.service.ts
+++ Backend/api-keuze-kompass-lu1/src/application/Services/module.service.ts
@@ -1,16 +1,16 @@
 import { Injectable, NotFoundException, Inject } from '@nestjs/common';
-import { Module as ModuleEntity } from '../../domain/entities/module.entity';
+import { Module } from '../../domain/entities/module.entity';
 import type { IModuleRepository } from '../../domain/repositories/i.module.repository';
 
 @Injectable()
 export class ModuleService {
   constructor(@Inject('IModuleRepository') private readonly moduleRepository: IModuleRepository) {}
 
-    async getAllModules(): Promise<ModuleEntity[]> {
+    async getAllModules(): Promise<Module[]> {
         return await this.moduleRepository.findAll();
     }
 
-    async getModuleById(id: string): Promise<ModuleEntity> {
+    async getModuleById(id: string): Promise<Module> {
         const module = await this.moduleRepository.findById(id);
         if (!module) {
             throw new NotFoundException(`Module with id ${id} not found`);
@@ -24,7 +24,7 @@ export class ModuleService {
         location?: string;
         level?: string;
         name?: string;
-    }): Promise<ModuleEntity[]> {
+    }): Promise<Module[]> {
         return await this.moduleRepository.findFiltered(filters);
     }
 
